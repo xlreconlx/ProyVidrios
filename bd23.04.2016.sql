7964e15f77626ae3866cd76467e61304eb49948d
william sanchez <wfsanchez53@misena.edu.co>
2016-05-05 10:53:19 -0500

    modificacion de la base de datos

--- bd23.04.2016.sql
+++ bd23.04.2016.sql
@@ -40,7 +40,7 @@ CREATE TABLE `abonodetalle` (
   KEY `fk_abonoDetalle_productos1_idx` (`idproductos`),
   CONSTRAINT `fk_abonoDetalle_abonos1` FOREIGN KEY (`idabonos`) REFERENCES `abonos` (`idabonos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_abonoDetalle_productos1` FOREIGN KEY (`idproductos`) REFERENCES `productos` (`idproductos`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,7 +49,7 @@ CREATE TABLE `abonodetalle` (
 
 LOCK TABLES `abonodetalle` WRITE;
 /*!40000 ALTER TABLE `abonodetalle` DISABLE KEYS */;
-INSERT INTO `abonodetalle` VALUES (1,1,2,'3 cuerpos 120*150',4,320255,1281020,50000,50,'120','150',NULL),(2,2,2,'3 cuerpos 120*150',1,359415,359415,60000,50,'120','150',NULL),(3,3,1,'2 cuerpos 210*234',1,389081,389081,45000,50,'210','234',NULL),(4,4,1,'2 cuerpos 123*123',1,236713,236713,45000,50,'123','123',NULL),(5,5,7,'vitrina 123*200 fondo: 50',1,787400,787400,50000,50,'123','200','50'),(6,6,5,'Puerta Aluminio T87 con Vidrio 190*110',6,282002,1692012,50000,50,'190','110','');
+INSERT INTO `abonodetalle` VALUES (1,1,2,'3 cuerpos 120*150',4,320255,1281020,50000,50,'120','150',NULL),(2,2,2,'3 cuerpos 120*150',1,359415,359415,60000,50,'120','150',NULL),(3,3,1,'2 cuerpos 210*234',1,389081,389081,45000,50,'210','234',NULL),(4,4,1,'2 cuerpos 123*123',1,236713,236713,45000,50,'123','123',NULL),(5,5,7,'vitrina 123*200 fondo: 50',1,787400,787400,50000,50,'123','200','50'),(6,6,5,'Puerta Aluminio T87 con Vidrio 190*110',6,282002,1692012,50000,50,'190','110',''),(7,7,1,'2 cuerpos 190*110',1,297009,297009,50000,50,'190','110',''),(8,8,1,'2 cuerpos 190*234',1,409319,409319,50000,50,'190','234','');
 /*!40000 ALTER TABLE `abonodetalle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -73,7 +73,7 @@ CREATE TABLE `abonos` (
   KEY `fk_abonos_empleado1_idx` (`idempleado`),
   CONSTRAINT `fk_abonos_cliente1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_abonos_empleado1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,7 +82,7 @@ CREATE TABLE `abonos` (
 
 LOCK TABLES `abonos` WRITE;
 /*!40000 ALTER TABLE `abonos` DISABLE KEYS */;
-INSERT INTO `abonos` VALUES (1,1,2,1281020,145500,1135520,'2016-03-23 02:02:56'),(2,1,2,359415,245000,93315,'2016-03-23 14:03:07'),(3,2,2,389081,34000,355081,'2016-04-12 19:45:51'),(4,1,2,236713,34000,-51887,'2016-04-14 05:00:54'),(5,1,2,787400,400000,387400,'2016-04-18 23:41:18'),(6,1,2,1692012,200000,1492012,'2016-04-28 18:16:40');
+INSERT INTO `abonos` VALUES (1,1,2,1281020,145500,1135520,'2016-03-23 02:02:56'),(2,1,2,359415,245000,93315,'2016-03-23 14:03:07'),(3,2,2,389081,34000,355081,'2016-04-12 19:45:51'),(4,1,2,236713,34000,-51887,'2016-04-14 05:00:54'),(5,1,2,787400,400000,387400,'2016-04-18 23:41:18'),(6,1,2,1692012,200000,1492012,'2016-04-28 18:16:40'),(7,1,2,297009,120000,127009,'2016-05-04 19:33:05'),(8,1,2,409319,45000,304319,'2016-05-04 19:35:40');
 /*!40000 ALTER TABLE `abonos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -105,7 +105,7 @@ CREATE TABLE `abonosecundario` (
   KEY `idempleado` (`idempleado`),
   CONSTRAINT `abonosecundario_ibfk_1` FOREIGN KEY (`idabonos`) REFERENCES `abonos` (`idabonos`),
   CONSTRAINT `abonosecundario_ibfk_2` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +114,7 @@ CREATE TABLE `abonosecundario` (
 
 LOCK TABLES `abonosecundario` WRITE;
 /*!40000 ALTER TABLE `abonosecundario` DISABLE KEYS */;
-INSERT INTO `abonosecundario` VALUES (1,2,1,3000,NULL,'2016-04-08 03:51:24'),(2,2,1,4000,NULL,'2016-04-09 00:26:22'),(3,2,1,4300,NULL,'2016-04-09 03:55:07'),(4,2,1,5000,93115,'2016-04-09 16:00:54'),(5,2,2,2400,93315,'2016-04-12 04:05:26'),(6,2,2,2400,90915,'2016-04-12 04:07:13'),(7,4,1,2300,198113,'2016-04-14 18:13:08'),(8,4,1,2300,195813,'2016-04-14 18:15:18'),(9,4,2,250000,-301887,'2016-04-14 23:37:01');
+INSERT INTO `abonosecundario` VALUES (1,2,1,3000,NULL,'2016-04-08 03:51:24'),(2,2,1,4000,NULL,'2016-04-09 00:26:22'),(3,2,1,4300,NULL,'2016-04-09 03:55:07'),(4,2,1,5000,93115,'2016-04-09 16:00:54'),(5,2,2,2400,93315,'2016-04-12 04:05:26'),(6,2,2,2400,90915,'2016-04-12 04:07:13'),(7,4,1,2300,198113,'2016-04-14 18:13:08'),(8,4,1,2300,195813,'2016-04-14 18:15:18'),(9,4,2,250000,-301887,'2016-04-14 23:37:01'),(10,7,1,50000,77009,'2016-05-04 19:34:09'),(11,8,1,60000,244319,'2016-05-04 19:36:36');
 /*!40000 ALTER TABLE `abonosecundario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -200,7 +200,7 @@ CREATE TABLE `cotizacion` (
   KEY `fk_cotizacion_cliente1_idx` (`idcliente`),
   CONSTRAINT `fk_compras_empleado1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_cotizacion_cliente1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,7 +209,7 @@ CREATE TABLE `cotizacion` (
 
 LOCK TABLES `cotizacion` WRITE;
 /*!40000 ALTER TABLE `cotizacion` DISABLE KEYS */;
-INSERT INTO `cotizacion` VALUES (1,1,2,1886402,359314,2245716,'2016-03-22 20:38:49'),(2,1,2,1550500,295333,1845833,'2016-03-22 21:13:42'),(3,1,2,1550500,295333,1845833,'2016-03-22 21:16:49'),(4,2,2,1222170,232794,1454964,'2016-03-23 14:00:51'),(5,1,2,1001270,190718,1191988,'2016-03-26 17:53:27'),(6,1,2,713297,135865,849162,'2016-04-14 02:51:19'),(7,1,2,713297,135865,849162,'2016-04-14 02:55:16'),(8,1,2,713297,135865,849162,'2016-04-14 02:56:46');
+INSERT INTO `cotizacion` VALUES (1,1,2,1886402,359314,2245716,'2016-03-22 20:38:49'),(2,1,2,1550500,295333,1845833,'2016-03-22 21:13:42'),(3,1,2,1550500,295333,1845833,'2016-03-22 21:16:49'),(4,2,2,1222170,232794,1454964,'2016-03-23 14:00:51'),(5,1,2,1001270,190718,1191988,'2016-03-26 17:53:27'),(6,1,2,713297,135865,849162,'2016-04-14 02:51:19'),(7,1,2,713297,135865,849162,'2016-04-14 02:55:16'),(8,1,2,713297,135865,849162,'2016-04-14 02:56:46'),(9,1,2,297250,56618,353868,'2016-05-03 04:13:18'),(10,1,2,297250,56618,353868,'2016-05-03 04:25:12'),(11,1,2,1069916,203793,1273709,'2016-05-05 15:06:30'),(12,1,2,1069916,203793,1273709,'2016-05-05 15:11:03'),(13,1,2,1069916,203793,1273709,'2016-05-05 15:13:05'),(14,1,2,1069916,203793,1273709,'2016-05-05 15:13:38'),(15,1,2,1069916,203793,1273709,'2016-05-05 15:17:48');
 /*!40000 ALTER TABLE `cotizacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -238,7 +238,7 @@ CREATE TABLE `cotizaciondetalle` (
   KEY `fk_cotizacionDetalle_cotizacion1_idx` (`idcotizacion`),
   CONSTRAINT `fk_cotizacionDetalle_cotizacion1` FOREIGN KEY (`idcotizacion`) REFERENCES `cotizacion` (`idcotizacion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_cotizacionDetalle_productos1` FOREIGN KEY (`idproductos`) REFERENCES `productos` (`idproductos`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -247,7 +247,7 @@ CREATE TABLE `cotizaciondetalle` (
 
 LOCK TABLES `cotizaciondetalle` WRITE;
 /*!40000 ALTER TABLE `cotizaciondetalle` DISABLE KEYS */;
-INSERT INTO `cotizaciondetalle` VALUES (3,1,1,'2 cuerpos 440*340',3,748572,2245716,50000,50,'440','340',NULL),(4,2,2,'3 cuerpos 134*239',3,407845,1223535,45000,50,'134','239',NULL),(5,2,2,'3 cuerpos 134*110',2,311149,622298,45000,50,'134','110',NULL),(6,3,2,'3 cuerpos 134*239',3,407845,1223535,45000,50,'134','239',NULL),(7,3,2,'3 cuerpos 134*110',2,311149,622298,45000,50,'134','110',NULL),(8,4,1,'2 cuerpos 440*240',2,727482,1454964,56000,50,'440','240',NULL),(9,5,2,'3 cuerpos 240*340',2,595994,1191988,50000,50,'240','340',NULL),(10,6,2,'3 cuerpos 234*123',2,424581,849162,45000,50,'234','123',NULL),(11,7,2,'3 cuerpos 234*123',2,424581,849162,45000,50,'234','123',NULL),(12,8,2,'3 cuerpos 234*123',2,424581,849162,45000,50,'234','123',NULL);
+INSERT INTO `cotizaciondetalle` VALUES (3,1,1,'2 cuerpos 440*340',3,748572,2245716,50000,50,'440','340',NULL),(4,2,2,'3 cuerpos 134*239',3,407845,1223535,45000,50,'134','239',NULL),(5,2,2,'3 cuerpos 134*110',2,311149,622298,45000,50,'134','110',NULL),(6,3,2,'3 cuerpos 134*239',3,407845,1223535,45000,50,'134','239',NULL),(7,3,2,'3 cuerpos 134*110',2,311149,622298,45000,50,'134','110',NULL),(8,4,1,'2 cuerpos 440*240',2,727482,1454964,56000,50,'440','240',NULL),(9,5,2,'3 cuerpos 240*340',2,595994,1191988,50000,50,'240','340',NULL),(10,6,2,'3 cuerpos 234*123',2,424581,849162,45000,50,'234','123',NULL),(11,7,2,'3 cuerpos 234*123',2,424581,849162,45000,50,'234','123',NULL),(12,8,2,'3 cuerpos 234*123',2,424581,849162,45000,50,'234','123',NULL),(13,9,7,'vitrina Eliot 110*120 fondo: 040',1,353868,353868,80000,50,'110','120','040'),(14,10,7,'vitrina Eliot 110*120 fondo: 040',1,353868,353868,80000,50,'110','120','040'),(15,11,5,'Puerta Aluminio T87 con Vidrio 220*110',1,467690,467690,50000,50,'220','110',''),(16,11,1,'2 cuerpos 220*110',1,325112,325112,50000,50,'220','110',''),(17,11,7,'vitrina Eliot 120*110 fondo: 040',1,480907,480907,50000,50,'120','110','040'),(18,12,5,'Puerta Aluminio T87 con Vidrio 220*110',1,467690,467690,50000,50,'220','110',''),(19,12,1,'2 cuerpos 220*110',1,325112,325112,50000,50,'220','110',''),(20,12,7,'vitrina Eliot 120*110 fondo: 040',1,480907,480907,50000,50,'120','110','040'),(21,13,5,'Puerta Aluminio T87 con Vidrio 220*110',1,467690,467690,50000,50,'220','110',''),(22,13,1,'2 cuerpos 220*110',1,325112,325112,50000,50,'220','110',''),(23,13,7,'vitrina Eliot 120*110 fondo: 040',1,480907,480907,50000,50,'120','110','040'),(24,14,5,'Puerta Aluminio T87 con Vidrio 220*110',1,467690,467690,50000,50,'220','110',''),(25,14,1,'2 cuerpos 220*110',1,325112,325112,50000,50,'220','110',''),(26,14,7,'vitrina Eliot 120*110 fondo: 040',1,480907,480907,50000,50,'120','110','040'),(27,15,5,'Puerta Aluminio T87 con Vidrio 220*110',1,467690,467690,50000,50,'220','110',''),(28,15,1,'2 cuerpos 220*110',1,325112,325112,50000,50,'220','110',''),(29,15,7,'vitrina Eliot 120*110 fondo: 040',1,480907,480907,50000,50,'120','110','040');
 /*!40000 ALTER TABLE `cotizaciondetalle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -338,7 +338,7 @@ CREATE TABLE `facturas` (
   KEY `fk_ventas_empleado1_idx` (`idempleado`),
   CONSTRAINT `fk_ventas_cliente` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_ventas_empleado1` FOREIGN KEY (`idempleado`) REFERENCES `empleado` (`idempleado`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -347,7 +347,7 @@ CREATE TABLE `facturas` (
 
 LOCK TABLES `facturas` WRITE;
 /*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
-INSERT INTO `facturas` VALUES (19,1,2,2833660,539744,3373404,'2016-03-22 22:35:56'),(20,2,2,2466597,469827,2936424,'2016-03-23 14:02:01'),(21,1,2,2977890,567216,3545106,'2016-04-14 23:34:01'),(22,1,2,1473804,280724,1754528,'2016-04-23 22:41:54'),(23,1,2,1473804,280724,1754528,'2016-04-23 22:44:15'),(24,1,2,1072434,204272,1276706,'2016-04-26 00:14:59');
+INSERT INTO `facturas` VALUES (19,1,2,2833660,539744,3373404,'2016-03-22 22:35:56'),(20,2,2,2466597,469827,2936424,'2016-03-23 14:02:01'),(21,1,2,2977890,567216,3545106,'2016-04-14 23:34:01'),(22,1,2,1473804,280724,1754528,'2016-04-23 22:41:54'),(23,1,2,1473804,280724,1754528,'2016-04-23 22:44:15'),(24,1,2,1072434,204272,1276706,'2016-04-26 00:14:59'),(28,1,2,249488,47521,297009,'2016-05-04 19:30:51');
 /*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -397,7 +397,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'2 cuerpos','ventana'),(2,'3 cuerpos','ventana'),(3,'4 cuerpos','ventana'),(4,'vidrio','vidrio'),(5,'Puerta Aluminio T87 con Vidrio','aluminio'),(6,'Puerta Aluminio T103 con entamborado U71','entamborada'),(7,'vitrina Eliot','vitrina'),(8,'Puerta Aluminio T87  con entamborado F06','aluminio y entamborado'),(9,'puerta Aluminio T103 con vidrio','aluminio y vidrio'),(10,'puerta Aluminio Alamo 1 1/2','aluminioAlamo y Vidrio'),(11,'vitrina PerfilEsquinero 1','vitrina corta'),(12,'vitrina PerfilEsquinero 1 Larga','vitrina larga');
+INSERT INTO `productos` VALUES (1,'Vitrina 2 cuerpos','ventana'),(2,'Vitrina 3 Cuerpos','ventana'),(3,'Vitrina 4 Cuerpos','ventana'),(4,'vidrio','vidrio'),(5,'Puerta Aluminio T87 con Vidrio','aluminio'),(6,'Puerta Aluminio T103 con entamborado U71','entamborada'),(7,'Vitrina Cuarto Circulo 1 1/2','vitrina'),(8,'Puerta Aluminio T87  con entamborado F06','aluminio y entamborado'),(9,'puerta Aluminio T103 con vidrio','aluminio y vidrio'),(10,'puerta Aluminio Alamo 1 1/2','aluminioAlamo y Vidrio'),(11,'vitrina PerfilEsquinero 1','vitrina corta'),(12,'vitrina PerfilEsquinero 1 Larga','vitrina larga');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -422,7 +422,7 @@ CREATE TABLE `puertas` (
 
 LOCK TABLES `puertas` WRITE;
 /*!40000 ALTER TABLE `puertas` DISABLE KEYS */;
-INSERT INTO `puertas` VALUES (1,'aluminio3conAleta',7334),(2,'aluminioT87',5333),(3,'partidorT103',12833),(4,'pisavidrios',4000),(5,'chapa',45000),(6,'pibotesAmericanos',10000),(7,'varillaTensora',5000),(8,'escuadras',500),(9,'perfilU71',10000),(10,'entamborado',10000),(11,'entamboradoF06',10000),(12,'Alamo UnoMedia',10000),(13,'bisagra',1800),(14,'empaque',1400);
+INSERT INTO `puertas` VALUES (1,'aluminio3conAleta',7334),(2,'aluminioT87',5333),(3,'partidorT103',12833),(4,'pisavidrios',4000),(5,'chapa',90000),(6,'pibotesAmericanos',30000),(7,'varillaTensora',5000),(8,'escuadras',500),(9,'perfilU71',6250),(10,'entamborado',12750),(11,'entamboradoF06',10000),(12,'Alamo UnoMedia',9700),(13,'bisagra',1800),(14,'empaque',1400);
 /*!40000 ALTER TABLE `puertas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -531,7 +531,7 @@ CREATE TABLE `ventanadetalle` (
   KEY `fk_ventanaDetalle_productos1_idx` (`idproductos`),
   CONSTRAINT `fk_ventanaDetalle_facturas1` FOREIGN KEY (`idfacturas`) REFERENCES `facturas` (`idfacturas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_ventanaDetalle_productos1` FOREIGN KEY (`idproductos`) REFERENCES `productos` (`idproductos`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -540,7 +540,7 @@ CREATE TABLE `ventanadetalle` (
 
 LOCK TABLES `ventanadetalle` WRITE;
 /*!40000 ALTER TABLE `ventanadetalle` DISABLE KEYS */;
-INSERT INTO `ventanadetalle` VALUES (39,19,2,'3 cuerpos 440*340',3,1124468,3373404,67000,50,'440','340',NULL),(40,20,3,'4 cuerpos 440*240',3,978808,2936424,55000,45,'440','240',NULL),(41,21,3,'4 cuerpos 123*345',4,501229,2004916,34400,50,'123','345',NULL),(42,21,3,'4 cuerpos 254*345',2,770095,1540190,34400,50,'254','345',NULL),(43,22,2,'3 cuerpos 345*354',2,877264,1754528,50000,50,'345','354',''),(44,23,2,'3 cuerpos 345*354',2,877264,1754528,50000,50,'345','354',''),(45,24,1,'2 cuerpos 345*354',2,638353,1276706,50000,50,'345','354','');
+INSERT INTO `ventanadetalle` VALUES (39,19,2,'3 cuerpos 440*340',3,1124468,3373404,67000,50,'440','340',NULL),(40,20,3,'4 cuerpos 440*240',3,978808,2936424,55000,45,'440','240',NULL),(41,21,3,'4 cuerpos 123*345',4,501229,2004916,34400,50,'123','345',NULL),(42,21,3,'4 cuerpos 254*345',2,770095,1540190,34400,50,'254','345',NULL),(43,22,2,'3 cuerpos 345*354',2,877264,1754528,50000,50,'345','354',''),(44,23,2,'3 cuerpos 345*354',2,877264,1754528,50000,50,'345','354',''),(45,24,1,'2 cuerpos 345*354',2,638353,1276706,50000,50,'345','354',''),(55,28,1,'2 cuerpos 190*110',1,297009,297009,50000,50,'190','110','');
 /*!40000 ALTER TABLE `ventanadetalle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -565,7 +565,7 @@ CREATE TABLE `vidrios` (
 
 LOCK TABLES `vidrios` WRITE;
 /*!40000 ALTER TABLE `vidrios` DISABLE KEYS */;
-INSERT INTO `vidrios` VALUES (1,'2mm',9624),(2,'2mm Antireflejo',20400),(3,'3mm',10684),(4,'4mm',16000),(5,'5mm ',22552),(6,'6mm ',26168),(7,'grabado',26800),(8,'azul ',25488),(9,'3mm Espejo',19850),(10,'4mm Espejo',22630);
+INSERT INTO `vidrios` VALUES (1,'2mm',9624),(2,'2mm Antireflejo',20400),(3,'3mm',10684),(4,'4mm',16001),(5,'5mm ',22552),(6,'6mm ',26168),(7,'grabado',26800),(8,'azul ',25488),(9,'3mm Espejo',19850),(10,'4mm Espejo',22630);
 /*!40000 ALTER TABLE `vidrios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -607,4 +607,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-29 15:25:17
+-- Dump completed on 2016-05-05 10:49:32
